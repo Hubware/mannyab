45ad8371d19637a11363d620a414a59ab0708a7f
codebox workspace user <arterzatij@gmail.com>
2014-04-03 21:56:33 +0000

    change port

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ var express = require('express');
 
 var app = express();
 
+var mensajes = [];
+
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
 
-app.listen(3000);
+app.get('/mensaje/new/:mensaje', function (req, res) {
+    mensajes.push(req.params.mensaje)
+    res.send('Tu mensaje fue: ' + req.params.mensaje);
+});
+
+app.get('/mensajes/', function (req, res) {
+    res.send(mensajes);
+});
+
+app.get('/mensajes', function (req, res) {
+    res.send('Tu mensaje fue: ' + req.params.mensaje);
+});
+
+app.listen(80);
 console.log('app working');
